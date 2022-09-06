1e77f4e83e338410c0fdd08c6ccb43a8f32c485f
tengwenfan <112731275+tengwenfan@users.noreply.github.com>
2022-09-06 08:56:10 -0700

    Add files via upload

---
+++ parcel1_ind.sh
@@ -0,0 +1,38 @@
+## Parcel1_30_subs
+
+
+/usr/local/fsl/bin/featquery 30 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_001/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_002/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_005/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_006/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_007/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_008/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_009/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_011/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_012/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_013/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_014/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_016/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_023/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_025/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_026/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_027/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_029/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_030/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_037/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_038/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_039/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_046/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_047/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_049/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_050/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_051/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_054/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_060/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_061/ASL.gfeat/cope2.feat 
+/Volumes/juschnei/lendlab/projects/sllip/sllip_nas/data/mri/derivatives/fsl/higherlevelanalysis/sllip_064/ASL.gfeat/cope2.feat 
+2  stats/zstat1 stats/zstat2 featquery_parcel1_Tengwen -p -s -b 
+/Volumes/juschnei/lendlab/projects/sllip/make_parcels-tengwen/parcels/My_Experiment_26-May-2020/parcel_1.nii.gz
+
+
